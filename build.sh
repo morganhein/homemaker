51268d1df609715f96a10d2f3c8dad4f36d6238a
Ricardo Tutume <ricardotutume@TutsMBPv2.local>
2019-09-04 14:13:45 -0300

    basd

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #/bin/bash
+rm -rf build
 GOOS=darwin GOARCH=amd64 && go build -o build/$GOOS/homemaker
-GOOS=linux GOARCH=amd64 && go build -o build/$GOOS/homemaker--$GOARCH
+GOOS=linux GOARCH=amd64 && go build -o build/$GOOS/homemaker
 GOOS=linux GOARCH=arm && go build -o build/$GOOS/homemaker-$GOARCH
 GOOS=windows GOARCH=amd64 && go build -o build/$GOOS/homemaker.exe
