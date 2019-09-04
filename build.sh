b6ebd27a03936ceb1b93e0fb59408c720864efd9
Tuts <github@tutume.net>
2019-09-04 14:25:55 -0300

    asdf

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #/bin/bash
 rm -rf build
-GOOS=darwin GOARCH=amd64 && go build -o build/$GOOS/homemaker
-GOOS=linux GOARCH=amd64 && go build -o build/$GOOS/homemaker
-GOOS=linux GOARCH=arm && go build -o build/$GOOS/homemaker-$GOARCH
-GOOS=windows GOARCH=amd64 && go build -o build/$GOOS/homemaker.exe
+gox -os="windows linux darwin" -arch="amd64" -output="build/{{.OS}}/{{.Arch}}/{{.Dir}}"
